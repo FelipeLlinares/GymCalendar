8c21b21b2ae9e78d669a349776387c081d78ec82
FelipeLlinares <80314302+FelipeLlinares@users.noreply.github.com>
2022-11-13 16:59:46 +0100

    Main activity 2 created

--- app/src/main/java/com/example/gymcalendar/MainActivity.java
+++ app/src/main/java/com/example/gymcalendar/MainActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private RutinaDbHelper dbHelper;
     private SQLiteDatabase db;
 
+    private CalendarView calendarView;
+    private Button btnVerDia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new RutinaDbHelper(getApplicationContext(), "myrutina.db");
+        db = dbHelper.getWritableDatabase();
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        btnVerDia = (Button) findViewById(R.id.btnVerDia) ;
+
+        RutinaContract.RutinaEntry.initRutina(db);
     }
 }
--- app/src/main/java/com/example/gymcalendar/RutinaContract.java
+++ app/src/main/java/com/example/gymcalendar/RutinaContract.java
@@ -1,5 +1,7 @@
 package com.example.gymcalendar;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 public class RutinaContract {
@@ -12,5 +14,14 @@ public class RutinaContract {
         public static final String COLUMN_NAME_SERIES = "series";
         public static final String COLUMN_NAME_REPETICIONES = "repeticiones";
         public static final String COLUMN_NAME_PESO = "peso";
+
+        public static void initRutina(SQLiteDatabase db){
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NAME_FECHA, "20/10/2022");
+            values.put(COLUMN_NAME_EJERCICIO, "Sentadilla");
+            db.insert(TABLE_NAME, null, values);
+        }
     }
+
+
 }
